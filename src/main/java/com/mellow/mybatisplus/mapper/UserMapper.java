b34e8599ebaec9fcea1f3f113c30d1c2b0a458f1
zhengxinyu <mellowzz@163.com>
2021-08-24 21:26:37 +0800

    mybatis-plus commit two master

--- src/main/java/com/mellow/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/mellow/mybatisplus/mapper/UserMapper.java
@@ -3,5 +3,10 @@ package com.mellow.mybatisplus.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mellow.mybatisplus.entity.User;
 
+import java.util.List;
+
 public interface UserMapper extends BaseMapper<User> {
+
+    // 自定义mapper
+    List<User> selectAllByName(String name);
 }
